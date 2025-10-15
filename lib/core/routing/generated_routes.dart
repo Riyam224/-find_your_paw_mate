bd394d812e46f81fa1896649211a2f927729ed45
riyam <cs50riyam211@gmail.com>
2025-10-15 11:08:10 +0300

    core(styles and routing) / features( setup needed features)  -   create app routes and app colors

---
+++ lib/core/routing/generated_routes.dart
@@ -0,0 +1,50 @@
+import 'package:animals_tasks/core/routing/app_routes.dart';
+import 'package:animals_tasks/features/details/presentation/screens/details_screen.dart';
+import 'package:animals_tasks/features/favorite/presentation/screens/favorite_screen.dart';
+import 'package:animals_tasks/features/home/presentation/screens/home_screen.dart';
+import 'package:animals_tasks/features/onboarding/presentation/screens/onboarding_screen.dart';
+import 'package:animals_tasks/features/splash/presentation/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class RouteGenerator {
+  static GoRouter mainRoutingInOurApp = GoRouter(
+    errorBuilder: (context, state) =>
+        const Scaffold(body: Center(child: Text('404 Not Found'))),
+    // todo initial route
+    initialLocation: AppRoutes.splash,
+    routes: [
+      GoRoute(
+        path: AppRoutes.splash,
+        name: AppRoutes.splash,
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: AppRoutes.onboarding,
+        name: AppRoutes.onboarding,
+        builder: (context, state) => const OnboardingScreen(),
+      ),
+
+      GoRoute(
+        path: AppRoutes.home,
+        name: AppRoutes.home,
+        builder: (context, state) => const HomeScreen(),
+      ),
+
+      GoRoute(
+        path: AppRoutes.details,
+        name: AppRoutes.details,
+        builder: (context, state) {
+          return DetailsScreen();
+        },
+      ),
+      GoRoute(
+        path: AppRoutes.favorite,
+        name: AppRoutes.favorite,
+        builder: (context, state) {
+          return FavoriteScreen();
+        },
+      ),
+    ],
+  );
+}
